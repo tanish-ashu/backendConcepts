b6559d3016d5b03a7703e53927a1ea35009afc1c
ashu2005rajak@gmail.com <ashu2005rajak@gmail.com>
2026-04-16 03:26:03 +0530

    Creating RestAPI for inter application , frontend and backend , req, res communication, and testing using Postman

--- day-3/server.js
+++ day-3/server.js
@@ -2,10 +2,23 @@ const express = require("express")
 
 const app = express()
 
-app.get('/', (req, res) =>{
-    res.send('HOME Page !')
+app.use(express.json())
+
+const notes = []
+
+app.post('/notes', (req, res) =>{
+
+  console.log(req.body)
+
+  notes.push(req.body)
+
+  res.send('note created')
 })
 
-app.listen(3000, ()=>{
-  console.log("server is running on port 3000")
+app.get("/notes", (req, res) =>{
+  res.send(notes)
+})
+
+app.listen(3000,() =>{
+  console.log("Server is running on port 3000")
 })
