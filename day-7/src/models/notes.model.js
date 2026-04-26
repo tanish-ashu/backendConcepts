2523e89b189fc4597f993c2cd16d553b0ae31071
ashu2005rajak@gmail.com <ashu2005rajak@gmail.com>
2026-04-27 00:39:20 +0530

    Defining database or cluster structure and fitting it to Model variable to use it for CURD

--- day-7/src/models/notes.model.js
+++ day-7/src/models/notes.model.js
@@ -1,12 +1,12 @@
-const mongoose = requrie("mongoose")
+const mongoose = require("mongoose")
 
-
+// defining structure
 const noteSchema = new mongoose.Schema({
   title: String,
   description: String,
 })
 
-
+// helps to perform CURD operations later -- Model variable which has whole model is required manditory for perfoming everything
 const noteModel = mongoose.model("notes", noteSchema) 
 
 module.exports = noteModel
