6e6a9b8fff0ccba7cdd5f47ca014cac76d27de64
ashu2005rajak@gmail.com <ashu2005rajak@gmail.com>
2026-06-13 19:47:02 +0530

    Schema for structure and model for operation Created

---
+++ day-9/Backend/src/models/node.model.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose")
+
+
+
+const noteSchema = new mongoose.Schema({
+  title: String,
+  description: String,
+})
+
+
+
+const noteModel = mongoose.model("notes", noteSchema)
+
+module.exports = noteModel
