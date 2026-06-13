d9cc012be2bad00470518251b54d355810349f72
ashu2005rajak@gmail.com <ashu2005rajak@gmail.com>
2026-06-14 00:10:43 +0530

    frontend working with backend connection after api creation
    FETCHING AND SHOWING

--- day-9/Frontend/src/App.jsx
+++ day-9/Frontend/src/App.jsx
@@ -1,31 +1,28 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './App.css'
+import axios from 'axios'
 
 const App = () => {
 
-  const [notes, setNotes] = useState([
-    {
-      title: "test title",
-      description: "test description"
-    },
-    {
-      title: "test title",
-      description: "test description"
-    },
-    {
-      title: "test title",
-      description: "test description"
-    },
-]);
+  const [notes, setNotes] = useState([]);
 
+  useEffect(() =>{
+    axios.get('http://localhost:3000/api/notes')
+    .then((res) =>{
+      setNotes(res.data.notes || [])
+    })
+    .catch(error =>{
+     console.error("Fetch note error:", error)
+    });
+  }, []);
 
   return (
     <>
     <div className='notes'>
       {
-        notes.map(note =>{
+        notes.map( (note,idx) =>{
         return <div 
-          className='note'>
+          className='note' key={idx}>
             <h1>{note.title}</h1>
             <p>{note.description}</p>
           </div>  
